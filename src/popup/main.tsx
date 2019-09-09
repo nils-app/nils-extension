7ec3025b6af6ea9ebf5eea083f1d0d7d0b39b46a
Alejandro U. Alvarez <aluralma@gmail.com>
2019-09-09 11:35:12 +0100

    initial setup

---
+++ src/popup/main.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import "./index.css"
+
+class Popup extends React.Component {
+    render() {
+        return (
+            <div className="popup-padded">
+                <h1>{ chrome.i18n.getMessage("l10nHello") }</h1>
+            </div>
+        )
+    }
+}
+
+// --------------
+
+ReactDOM.render(
+    <Popup />,
+    document.getElementById('root')
+)
