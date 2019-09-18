8d7bc47e79eea412df610f7864f811fd734cc8f7
Alejandro U. Alvarez <aluralma@gmail.com>
2019-09-18 08:30:58 +0100

    cleanup

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,45 +1,34 @@
 chrome.tabs.onActivated.addListener((info) => {
   chrome.tabs.get(info.tabId, (change) => {
-    console.log('loaded', change.url);
-    if (change.url === undefined) {
-      chrome.browserAction.setPopup({ tabId: info.tabId, popup: '' })
-      chrome.browserAction.setIcon({
-        path: 'icons/icon512-gray.png',
-        tabId: info.tabId
-      })
-      console.log('tab not defined')
-    } else if (change.url.match(/https:\/\/(www\.)?google\.com\/*/) == null) {
-      chrome.browserAction.setPopup({ tabId: info.tabId, popup: '' })
-      chrome.browserAction.setIcon({
-        path: 'icons/icon512-green.png',
-        tabId: info.tabId
-      })
-      console.log('tab is not google')
-    } else {
-      chrome.browserAction.setPopup({
-        tabId: info.tabId,
-        popup: 'html/popup.html'
-      })
-      chrome.browserAction.setIcon({
-        path: 'icons/icon512-orange.png',
-        tabId: info.tabId
-      })
-      console.log('tab is google')
-    }
+    onLoad(info.tabId, change.url)
   })
 })
-// chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
-//   if (tab.url === undefined) {
-//     return
-//   } else if (tab.url.match(/https:\/\/google\.com\/*/) == null) {
-//     chrome.browserAction.setPopup({ tabId: tabId, popup: '' })
-//     chrome.browserAction.setIcon({
-//       path: 'icons/icon512-green.png',
-//       tabId: tabId
-//     })
-//     console.log('updated: not google')
-//   } else {
-//     chrome.browserAction.setIcon({ path: 'icons/icon512-orange.png', tabId: tabId })
-//     console.log('updated: google')
-//   }
-// })
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+  onLoad(tabId, tab.url)
+})
+
+const onLoad = (tabId: number, url: string) => {
+  if (url === undefined) {
+    chrome.browserAction.setIcon({
+      path: 'icons/icon512-gray.png',
+      tabId,
+    })
+    console.log('tab not defined')
+  } else if (url.match(/https:\/\/(www\.)?google\.com\/*/) == null) {
+    chrome.browserAction.setIcon({
+      path: 'icons/icon512-green.png',
+      tabId,
+    })
+    console.log('tab is not google')
+  } else {
+    chrome.browserAction.setPopup({
+      tabId,
+      popup: 'html/popup.html'
+    })
+    chrome.browserAction.setIcon({
+      path: 'icons/icon512-orange.png',
+      tabId,
+    })
+    console.log('tab is google')
+  }
+};
