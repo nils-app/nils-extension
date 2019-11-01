f83f05e8ed67be26cb23c30b102a2801698e7b8f
Alejandro U. Alvarez <aluralma@gmail.com>
2019-11-01 08:37:11 +0000

    initial refactoring to use create react app

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+import Popup from './Popup';
+import 'assets/app.scss';
+
+const app = (
+    <Popup />
+);
+
+ReactDOM.render(app, document.getElementById("root"));
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,97 +1,32 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import classnames from 'classnames';
-import { browser } from 'webextension-polyfill-ts';
-import { ButtonToolbar, Button, Container, Row, Col, Alert } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { browser } from "webextension-polyfill-ts";
 
-import { getUrlStatus, TabStatus } from '../lib/tab';
+import { TabStatus, getUrlStatus } from "../lib/tabs";
 
-import './index.scss';
+export default () => {
+  const [ tabStatus, setTabStatus ] = useState<TabStatus | null>(null);
 
-type State = {
-  tabStatus: TabStatus,
+  useEffect(() => {
+    browser.tabs.query({ active: true, currentWindow: true }).then(
+      tabs => {
+        if (tabs.length < 1 || !tabs[0].url) {
+          console.log(" no tab!");
+          return null;
+        }
+        setTabStatus(
+          getUrlStatus(tabs[0].url)
+        );
+      },
+      () => {
+        setTabStatus(null);
+      }
+    );
+  }, []);
+
+  return (
+    <pre className='code'>
+      <p>Popup content test</p>
+      <p>{ JSON.stringify(tabStatus, null, 2) }</p>
+    </pre>
+  );
 };
-
-class Popup extends React.Component {
-  state: State = {
-    tabStatus: null,
-  };
-
-  getCurrentTab () {
-    browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
-      if (tabs.length < 1) {
-        console.log(' no tab!');
-        return null;
-      }
-      this.setState({
-        tabStatus: getUrlStatus(tabs[0].url),
-      });
-    }, () => {
-      this.setState({
-        tabStatus: null,
-      });
-    });
-  }
-
-  componentDidMount () {
-    this.getCurrentTab();
-  }
-
-  renderTabStatus () {
-    let content = 'Unsupported';
-    let className = 'unsupported';
-
-    if (this.state.tabStatus) {
-      const { status, amount } = this.state.tabStatus;
-      className = status;
-      if (status === 'blocked') {
-        content = 'Blocked';
-      } else if (status === 'paid') {
-        const s = amount > 1 ? 's' : '';
-        content = `Sent ${amount} Nil${s}!`;
-      } else {
-        content = 'Unsupported';
-      }
-    }
-
-    console.log('status', this.state.tabStatus);
-
-    return (
-      <div className={classnames('status', className)}>
-        {content}
-      </div>
-    );
-  }
-
-  render () {
-    return (
-      <div className='popup'>
-        {this.renderTabStatus()}
-        <Container>
-          <Row>
-            <Col>
-              <Alert variant='primary'>
-                <Alert.Heading>Balance</Alert.Heading>
-                <p>100 Nils</p>
-              </Alert>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <ButtonToolbar className='spaced'>
-                <Button variant='outline-danger' size='sm'>
-                  Block
-                </Button>
-                <Button variant='outline-primary' size='sm'>
-                  Add balance
-                </Button>
-              </ButtonToolbar>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Popup />, document.getElementById('root'));
